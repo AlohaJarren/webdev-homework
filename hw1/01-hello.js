739b7cb5251971e5f8032e2e70622e80bcbde0ac
Jarren Calizo <calizo.jarren@gmail.com>
2025-04-17 18:41:03 -0700

    Logged personal bio message to console on page load

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -3,6 +3,6 @@ window.addEventListener("load", loadPage);
 function loadPage(event) {
   console.log("The page has now loaded.");
   console.log(
-    "My name is Jarren Calizo, and I'm a first-year Computer Science graduate student at Portland State. I’m passionate about cybersecurity, web development, and building creative tech projects that balance form, function, and accessibility. This course is a way for me to sharpen my front-end skills and revisit the side of code that first got me excited about tech in high school."
+    "My name is Jarren Calizo, and I'm a first-year Computer Science graduate student at Portland State. I'm passionate about cybersecurity, web development, and building creative tech projects that balance form, function, and accessibility. This course is a way for me to sharpen my front-end skills and revisit the side of code that first got me excited about tech in high school."
   );
 }
